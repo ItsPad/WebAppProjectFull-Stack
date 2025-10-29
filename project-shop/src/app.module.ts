86c788a51a471603448ca890d29dc7be1b6d06b9
ItsPad <patape6742@gmail.com>
2025-10-30 02:22:59 +0700

    backend CRUD completly

--- project-shop/src/app.module.ts
+++ project-shop/src/app.module.ts
@@ -4,9 +4,8 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 
-
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://patape6742_db_user:pzO0KDwfuiHVxyQx@cluster0.hqijale.mongodb.net/')],
+  imports: [MongooseModule.forRoot('mongodb+srv://patape6742_db_user:pzO0KDwfuiHVxyQx@cluster0.hqijale.mongodb.net/testproducts'), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- project-shop/src/products/dto/create-product.dto.ts
+++ project-shop/src/products/dto/create-product.dto.ts
@@ -1,1 +1,6 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+    readonly name: string;
+    readonly price: number;
+    readonly description?: string;
+    readonly amount : number;
+}
