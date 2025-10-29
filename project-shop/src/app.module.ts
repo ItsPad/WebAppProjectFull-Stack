3e37d7d05983a76fc01276806ad15ffbe06dc745
ItsPad <patape6742@gmail.com>
2025-10-30 00:43:36 +0700

    update mongodb

--- project-shop/src/app.module.ts
+++ project-shop/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://patape6742_db_user:pzO0KDwfuiHVxyQx@cluster0.hqijale.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
